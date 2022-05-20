92a58c3677321e30dc51c488ef3eb15ab0f1086e
rushanthan03 <prushanthan@gmail.com>
2022-05-20 13:20:35 +0530

    add employee

--- src/contents/Master/Employee/Employee.interface.ts
+++ src/contents/Master/Employee/Employee.interface.ts
@@ -1,7 +1,25 @@
 interface IEmployee {
   id?: number;
-  name?: string;
   address?: string;
+  lastName?: string;
+  firstName?: string;
+  dateOfBirth?: String;
+  email?: string;
+  gender?: string;
+  contactNo?: string;
+  maritalStatus?: string;
+  nationality?: string;
+  religon?: string;
+  passportNo?: string;
+  drivingLicenceNo?: string;
+  bloodGroup?: string;
+
+  description?: string;
+    joinDate?: string;
+    companyLocation?: string;
+    name?: string;
+    employmentType?: string;
+    businessUnit?: string;
 }
 
 export type { IEmployee };
