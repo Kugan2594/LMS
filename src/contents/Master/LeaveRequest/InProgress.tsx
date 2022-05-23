fb21359fe48ca63e3e2c7204b90499417f38c4c0
unknown <kugan2594@gmail.com>
2022-05-23 17:49:41 +0100

    button off

--- src/contents/Master/LeaveRequest/InProgress.tsx
+++ src/contents/Master/LeaveRequest/InProgress.tsx
@@ -1,10 +1,10 @@
 import {
-  Button,
-  Card,
-  CardContent,
-  Container,
-  Divider,
-  Typography,
+    Button,
+    Card,
+    CardContent,
+    Container,
+    Divider,
+    Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import Modals from "src/components/atoms/Modals";
@@ -36,11 +36,13 @@ let mockData = [
         toDate: "15/06/2022",
         requestedDate: "19/05/2022",
         status: "Approved",
-        approvers: [{names: "KugApp1", appStatus: "Approved"},
-        {names: "KugApp2", appStatus: "Approved"}, 
-        {names: "KugApp3", appStatus: "Approved"},],
-      },
-      {
+        approvers: [
+            { names: "KugApp1", appStatus: "Approved" },
+            { names: "KugApp2", appStatus: "Approved" },
+            { names: "KugApp3", appStatus: "Approved" },
+        ],
+    },
+    {
         id: 2,
         employeeId: 2,
         employeeName: "Cudeson Cudeson",
@@ -51,12 +53,14 @@ let mockData = [
         toDate: "09/06/2022",
         requestedDate: "18/05/2022",
         status: "Rejected",
-        approvers: [{names: "CudApp1", appStatus: "Approved"},
-        {names: "CudApp2", appStatus: "Approved"},
-        {names: "CudApp3", appStatus: "Rejected"},
-        {names: "CudApp4", appStatus: "Rejected"},],
-      },
-      {
+        approvers: [
+            { names: "CudApp1", appStatus: "Approved" },
+            { names: "CudApp2", appStatus: "Approved" },
+            { names: "CudApp3", appStatus: "Rejected" },
+            { names: "CudApp4", appStatus: "Rejected" },
+        ],
+    },
+    {
         id: 3,
         employeeId: 3,
         employeeName: "Kuganesan Kuganesan",
@@ -67,11 +71,13 @@ let mockData = [
         toDate: "15/06/2022",
         requestedDate: "19/05/2022",
         status: "Approved",
-        approvers: [{names: "KugApp1", appStatus: "Approved"},
-        {names: "KugApp2", appStatus: "Approved"}, 
-        {names: "KugApp3", appStatus: "Approved"},],
-      },
-      {
+        approvers: [
+            { names: "KugApp1", appStatus: "Approved" },
+            { names: "KugApp2", appStatus: "Approved" },
+            { names: "KugApp3", appStatus: "Approved" },
+        ],
+    },
+    {
         id: 4,
         employeeId: 4,
         employeeName: "Cudeson Cudeson",
@@ -82,203 +88,211 @@ let mockData = [
         toDate: "09/06/2022",
         requestedDate: "18/05/2022",
         status: "Rejected",
-        approvers: [{names: "CudApp1", appStatus: "Approved"},
-        {names: "CudApp2", appStatus: "Approved"},
-        {names: "CudApp3", appStatus: "Approved"},
-        {names: "CudApp4", appStatus: "Rejected"},],
-      },
+        approvers: [
+            { names: "CudApp1", appStatus: "Approved" },
+            { names: "CudApp2", appStatus: "Approved" },
+            { names: "CudApp3", appStatus: "Approved" },
+            { names: "CudApp4", appStatus: "Rejected" },
+        ],
+    },
 ];
 
 function InProgress() {
-  const [pagination, setpagination] = useState({
-    pageNumber: 0,
-    pageSize: 10,
-    total: 0,
-  });
-  const [open, setOpen] = useState(false);
-  const [openDetails, setOpenDetails] = useState(false);
-  const [searchFields, setsearchFields] = useState({ name: "" });
-  const [sortField, setsortField] = React.useState({
-    sortField: "id",
-    direction: "DESC",
-  });
-  const [update, setUpdate] = useState(false);
-  const handleUpdate = (value) => {
-    setUpdate(true);
-  };
-  const handleUpdateClose = (value) => {
-    setUpdate(false);
-  };
+    const [pagination, setpagination] = useState({
+        pageNumber: 0,
+        pageSize: 10,
+        total: 0,
+    });
+    const [open, setOpen] = useState(false);
+    const [openDetails, setOpenDetails] = useState(false);
+    const [searchFields, setsearchFields] = useState({ name: "" });
+    const [sortField, setsortField] = React.useState({
+        sortField: "id",
+        direction: "DESC",
+    });
+    const [update, setUpdate] = useState(false);
+    const handleUpdate = (value) => {
+        setUpdate(true);
+    };
+    const handleUpdateClose = (value) => {
+        setUpdate(false);
+    };
 
-  const [leaveDetails, setLeaveDetails] = useState({});
+    const [leaveDetails, setLeaveDetails] = useState({});
 
-  const handleOpenLeaveDetails = (value) => {
-    setOpenDetails(true);
-    setLeaveDetails(value);
-  };
+    const handleOpenLeaveDetails = (value) => {
+        setOpenDetails(true);
+        setLeaveDetails(value);
+    };
 
-  const handleClickOpen = (value) => {
-    setOpen(true);
-  };
+    const handleClickOpen = (value) => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-    setOpenDetails(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+        setOpenDetails(false);
+    };
 
-  const onChangePage = (pageNumber, pageSize) => {};
+    const onChangePage = (pageNumber, pageSize) => {};
 
-  const onTableSearch = (values, sortField) => {};
+    const onTableSearch = (values, sortField) => {};
 
-  const columns: Column[] = [
-    {
-      id: "leaveType",
-      label: "LeaveType",
-      minWidth: 0,
-    },
-    {
-      id: "fromDate",
-      label: "FromDate",
-      minWidth: 0,
-    },
-    {
-      id: "toDate",
-      label: "ToDate",
-      minWidth: 0,
-    },
-    {
-      id: "reason",
-      label: "Reason",
-      minWidth: 0,
-    },
-    {
-      id: "leaveDays",
-      label: "Days",
-      minWidth: 0,
-    },
-    {
-      id: "status",
-      label: "Status",
-      minWidth: 0,
-    },
-    {
-      id: "details",
-      label: "",
-      minWidth: 40,
-      render: (value) => (
-        <Button
-        variant="text"
-        size="small"
-        onClick={() => handleOpenLeaveDetails(value)}>
-        Detail
-      </Button>
-      ),
-    },
-    {
-      id: "cancel",
-      label: "",
-      render: (value: any) => (
-        <Button
-          variant="text"
-          size="small"
-          onClick={() => {
-            handleClickOpen(value);
-          }}
-        >
-          Cancel
-        </Button>
-      ),
-      minWidth: 0,
-    },
-    {
-      id: "update",
-      label: "",
-      render: (value: any) => (
-        <Button
-          variant="text"
-          size="small"
-          onClick={() => {
-            handleUpdate(value);
-          }}
-        >
-          Update
-        </Button>
-      ),
-      minWidth: 0,
-    },
-  ];
+    const columns: Column[] = [
+        {
+            id: "leaveType",
+            label: "LeaveType",
+            minWidth: 0,
+        },
+        {
+            id: "fromDate",
+            label: "FromDate",
+            minWidth: 0,
+        },
+        {
+            id: "toDate",
+            label: "ToDate",
+            minWidth: 0,
+        },
+        {
+            id: "reason",
+            label: "Reason",
+            minWidth: 0,
+        },
+        {
+            id: "leaveDays",
+            label: "Days",
+            minWidth: 0,
+        },
+        {
+            id: "status",
+            label: "Status",
+            minWidth: 0,
+        },
+        {
+            id: "details",
+            label: "",
+            minWidth: 40,
+            render: (value) => (
+                <Button
+                    variant="text"
+                    size="small"
+                    onClick={() => handleOpenLeaveDetails(value)}
+                >
+                    Detail
+                </Button>
+            ),
+        },
+        {
+            id: "cancel",
+            label: "",
+            render: (value: any) => (
+                <Button
+                    variant="text"
+                    size="small"
+                    onClick={() => {
+                        handleClickOpen(value);
+                    }}
+                >
+                    Cancel
+                </Button>
+            ),
+            minWidth: 0,
+        },
+        {
+            id: "update",
+            label: "",
+            render: (value: any) => (
+                <Button
+                    variant="text"
+                    size="small"
+                    onClick={() => {
+                        handleUpdate(value);
+                    }}
+                >
+                    Update
+                </Button>
+            ),
+            minWidth: 0,
+        },
+    ];
 
-  return (
-    <div>
-      <Container maxWidth="lg">
-        <Card>
-          <CardContent>
-            <PageTitleWrapper>
-              <PageTitle heading="In progress" isButton={false} />
-            </PageTitleWrapper>
-            <Divider />
-
-            <Tables
-              columns={columns}
-              tableData={mockData}
-              onChangePage={onChangePage}
-              pageNumber={pagination.pageNumber}
-              total={pagination.total}
-              pageSize={pagination.pageSize}
-              searchFields={{}}
-              onTableSearch={onTableSearch}
-            />
-          </CardContent>
-        </Card>
+    return (
         <div>
-          <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              {"Are you sure Do you want to cancel Request?"}
-            </DialogTitle>
-            <DialogActions>
-              <Button onClick={handleClose}>No</Button>
-              <Button onClick={handleClose} autoFocus>
-                Yes
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-        <div>
-          <Dialog
-            open={openDetails}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            maxWidth="md"
-          >
-            <DialogTitle id="alert-dialog-title">{""}</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                <ViewHistory
-                  details={leaveDetails}
-                  isEmployeeDetail={true}
-                  isResponseButtons={true}
-                  cancel={handleClose}
+            <Container maxWidth="lg">
+                <Card>
+                    <CardContent>
+                        <PageTitleWrapper>
+                            <PageTitle heading="In progress" isButton={false} />
+                        </PageTitleWrapper>
+                        <Divider />
+
+                        <Tables
+                            columns={columns}
+                            tableData={mockData}
+                            onChangePage={onChangePage}
+                            pageNumber={pagination.pageNumber}
+                            total={pagination.total}
+                            pageSize={pagination.pageSize}
+                            searchFields={{}}
+                            onTableSearch={onTableSearch}
+                        />
+                    </CardContent>
+                </Card>
+                <div>
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Are you sure Do you want to cancel Request?"}
+                        </DialogTitle>
+                        <DialogActions>
+                            <Button onClick={handleClose}>No</Button>
+                            <Button onClick={handleClose} autoFocus>
+                                Yes
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+                <div>
+                    <Dialog
+                        open={openDetails}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                        maxWidth="md"
+                    >
+                        <DialogTitle id="alert-dialog-title">{""}</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                <ViewHistory
+                                    details={leaveDetails}
+                                    isEmployeeDetail={false}
+                                    isResponseButtons={false}
+                                    cancel={handleClose}
+                                />
+                            </DialogContentText>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <Modals
+                    modalTitle="Update Leave Request"
+                    modalWidth="60%"
+                    open={update}
+                    onClose={handleUpdateClose}
+                    modalBody={
+                        <LeaveRequestForm
+                            isButton={true}
+                            isButtonThree={true}
+                        />
+                    }
                 />
-              </DialogContentText>
-            </DialogContent>
-          </Dialog>
+            </Container>
         </div>
-        <Modals
-          modalTitle="Update Leave Request"
-          modalWidth="60%"
-          open={update}
-          onClose={handleUpdateClose}
-          modalBody={<LeaveRequestForm isButton={true} isButtonThree={true} />}
-        />
-      </Container>
-    </div>
-  );
+    );
 }
 
 export default InProgress;
