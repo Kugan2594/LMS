5b44ecd55146cf93bb6c1ebd73f56c899b26e1a1
unknown <sritharankeerthana1997@gmail.com>
2022-05-25 09:44:24 +0530

    designation integration

--- src/contents/Master/Designations/DesignationsInterface.ts
+++ src/contents/Master/Designations/DesignationsInterface.ts
@@ -1,18 +1,7 @@
 import { type } from "os";
 
 interface IDesignations {
-    leaveType?: string;
-    fromDate?: string;
-    toDate?: string;
-    reason?: string;
-    days?: number;
-    employee?: string;
-    status?: "Approved" | "Reject" | "New";
-    cancel?: any;
-    enableTitle?: boolean;
-    isButton?: boolean;
-    isButtonTwo?: boolean;
-    isButtonThree?: boolean;
-    isButtonFour?: boolean;
+    id?: number,
+    name?: string
 }
 export type { IDesignations };
