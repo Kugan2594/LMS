98fdcdd5a4cafcf671266e99a762c3bbb31701a7
rushanthan03 <prushanthan@gmail.com>
2022-06-03 11:37:53 +0530

    change

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -27,7 +27,6 @@ import { element } from "prop-types";
 
 import ManageLieuRequest from "src/contents/Master/LieuRequest/ManageLieuRequest";
 import ManageNotification from "src/contents/Master/Notification/ManageNotification";
-import Profile1 from "src/contents/profile/Profile1";
 const Loader = (Component: any) => (props: any) =>
     (
         <Suspense fallback={<SuspenseLoader />}>
@@ -48,10 +47,7 @@ const PrivateRoute: PartialRouteObject[] = [
                 path: "/profile",
                 element: <Profile />,
             },
-            {
-                path: "/profile1",
-                element: <Profile1 />,
-            },
+           
         ]
     },
     {
