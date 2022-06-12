c913cb67688ecdbf1b00b8d79ac4348e894c8dbb
unknown <sritharankeerthana1997@gmail.com>
2022-06-12 15:02:51 +0530

    leave allocated days changes

--- src/contents/Master/AllocationDays/ManageAllocateDay.tsx
+++ src/contents/Master/AllocationDays/ManageAllocateDay.tsx
@@ -188,7 +188,7 @@ function ManageAllocateDay() {
                         <DoughnutChart
                           selectedValue={post.remainingDays}
                           maxValue={
-                            post.allocatedDays === 0 ? 100 : post.allocatedDays
+                            post.allocatedDays === 0||0.5 ? 100 : post.allocatedDays
                           }
                           radius={60}
                           strokeWidth={12}
